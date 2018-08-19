8cdf2fb60d3df2990fd181181ac23fccb13a012d
Sandra <snelson3671@yahoo.com>
2018-08-18 21:35:55 -0500

    CG 6-2 Upper Case

--- CG 6-2 Upper Case/Program.cs
+++ CG 6-2 Upper Case/Program.cs
@@ -6,7 +6,13 @@ namespace CG_6_2_Upper_Case
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please type in your favorite quote.  ");
+            string quote = Console.ReadLine();
+
+            
+            Console.WriteLine(quote.ToUpper());
+
+            Console.ReadLine();
         }
     }
 }
